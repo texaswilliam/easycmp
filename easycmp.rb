0aa83ea27db61527de621bc51da725942f7dcf38
William Pyburn <texas.william@gmail.com>
2011-07-07 03:08:40 -0500

    Got ahead of myself: set instead of get.

--- easycmp.rb
+++ easycmp.rb
@@ -8,7 +8,7 @@ class Module
         class_variable_get(:@@easycmp_fields)|fields)
     
     define_method(:<=>) do |oth|
-      for field in self.class.class_variable_set(:@@easycmp_fields)
+      for field in self.class.class_variable_get(:@@easycmp_fields)
         call=field.to_s.start_with?(?@) ? :instance_variable_get : :send
         result=self.send(call,field)<=>oth.send(call,field)
         return result if result.nonzero?

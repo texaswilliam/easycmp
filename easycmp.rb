f8371b4d4243fe5049c29ac1d8f6658f639d289e
William Pyburn <texas.william@gmail.com>
2011-07-07 02:54:32 -0500

    Fatal bug fix: fields stored as Module class vars.
    This fix includes a lot of tidying in the main rb. module_eval is used
    once to cut down on horrifically unreadable class_variable spaghetti,
    but in the end, the class_variable stuff is more responsible: if you're
    going to use module_eval, you should put __FILE__ and __LINE__ for the
    2nd and 3rd arguments so your eval can be debugged worth a damn.

--- easycmp.rb
+++ easycmp.rb
@@ -1,9 +1,14 @@
 class Module
   def easy_cmp *fields
-    @@easycmp_fields||=[]
-    @@easycmp_fields|=fields
+    return unless fields
+    fields=fields.flatten.collect{|field| field.to_sym}
+    
+    class_eval '@@easycmp_fields||=[]', __FILE__, __LINE__
+    class_variable_set(:@@easycmp_fields,
+        class_variable_get(:@@easycmp_fields)|fields)
+    
     define_method(:<=>) do |oth|
-      for field in @@easycmp_fields
+      for field in self.class.class_variable_set(:@@easycmp_fields)
         call=field.to_s.start_with?(?@) ? :instance_variable_get : :send
         result=self.send(call,field)<=>oth.send(call,field)
         return result if result.nonzero?

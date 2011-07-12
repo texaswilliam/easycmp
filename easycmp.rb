120c55571bd42c75b80d42e8e029a3629bea71aa
William Pyburn <texas.william@gmail.com>
2011-07-11 19:31:54 -0500

    Moved back to the module architecture.
    This is in preparation for adding an option for reversing comparators
    along with a new syntax using [] to enable it.

--- easycmp.rb
+++ easycmp.rb
@@ -1,23 +1,42 @@
-class Module
-  def easy_cmp *fields
+module EasyCmp
+  def self.add_fields klass, *fields
     return unless fields
-    fields=fields.flatten.collect{|field| field.to_sym}
     
-    #class_eval '@@easycmp_fields||=[]', __FILE__, __LINE__
-    unless instance_variable_defined?(:@easycmp_fields)
-      instance_variable_set(:@easycmp_fields,fields)
-    else
-      instance_variable_set(:@easycmp_fields,
-          instance_variable_get(:@easycmp_fields)|fields)
-    end
-    
-    define_method(:<=>) do |oth|
-      for field in self.class.instance_variable_get(:@easycmp_fields)
-        call=field.to_s.start_with?(?@) ? :instance_variable_get : :send
-        result=self.send(call,field)<=>oth.send(call,field)
-        return result if result.nonzero?
+    fields.flatten!
+    fields.collect!{|field| field.to_sym}
+
+    klass.class_exec fields do |fields|
+      unless instance_variable_defined?(:@easycmp_fields)
+        instance_variable_set(:@easycmp_fields,fields)
+      else
+        instance_variable_set(:@easycmp_fields,
+            instance_variable_get(:@easycmp_fields)|fields)
       end
-      0
+    end
+    return klass
+  end
+  def self.add_method klass
+    klass.class_exec do
+      def <=> oth
+        for field in self.class.instance_variable_get(:@easycmp_fields)
+          call=field.to_s.start_with?(?@) ? :instance_variable_get : :send
+          result=self.send(call,field)<=>oth.send(call,field)
+          return result if result.nonzero?
+        end
+        return 0
+      end
+    end
+    return klass
+  end
+
+  module ClassMethods
+    def easy_cmp *fields
+      EasyCmp.add_fields self, fields
+      EasyCmp.add_method self
     end
   end
+end
+
+class Object
+  extend EasyCmp::ClassMethods
 end

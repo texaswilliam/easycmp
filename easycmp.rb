49ae5fed24535926c40279af460b7c769a00fa55
William Pyburn <texas.william@gmail.com>
2011-07-11 23:02:01 -0500

    Added ability to specify options to easy_cmp.
    None of them work yet, but there will be an :append to specify if fields
    should be cleared or not before adding the given fields (defaults to
    true) and a :reverse that will allow one to compare fields given that
    way backwards (like multiplying the <=> by -1 when comparing fields
    marked :reverse or switching the left and right operands to <=> when
    doing the nested comparison).
    
    Commits adding these will add examples to the README.

--- easycmp.rb
+++ easycmp.rb
@@ -1,15 +1,15 @@
 module EasyCmp
-  def self.add_fields klass, *fields
-    return unless fields
-    
-    fields.flatten!
-    fields.collect!{|field| field.to_sym}
-
+  def self.process klass, fields, opts={}
+    add_fields klass, Hash[fields.collect{|field| [field,opts.clone]}]
+    add_method klass
+  end
+  def self.add_fields klass, fields
     unless klass.instance_variable_defined?(:@easycmp_fields)
       klass.instance_variable_set(:@easycmp_fields,fields)
     else
-      klass.instance_variable_set(:@easycmp_fields,
-          klass.instance_variable_get(:@easycmp_fields)|fields)
+      #if field exists, merge its old options with the new ones
+      klass.instance_variable_get(:@easycmp_fields)
+          .merge!(fields) {|key,old,new| old.merge(new)}
     end
 
     return klass
@@ -17,7 +17,7 @@ module EasyCmp
   def self.add_method klass
     klass.class_exec do
       def <=> oth
-        for field in self.class.instance_variable_get(:@easycmp_fields)
+        for field,opts in self.class.instance_variable_get(:@easycmp_fields)
           call=field.to_s.start_with?(?@) ? :instance_variable_get : :send
           result=self.send(call,field)<=>oth.send(call,field)
           return result if result.nonzero?
@@ -31,8 +31,10 @@ module EasyCmp
   module ClassMethods
     private
     def easy_cmp *fields
-      EasyCmp.add_fields self, fields
-      EasyCmp.add_method self
+      return unless fields
+      opts=fields.last.is_a?(Hash) ? fields.pop : {}
+      return if fields.empty?
+      EasyCmp.process self, fields.flatten.collect{|field| field.to_sym}, opts
     end
   end
 end
